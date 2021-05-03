09e8a118b44c156aaa974934d38706b7015e0aea
Lokesh118 <lokeshnelapati12@gmail.com>
2021-05-03 17:34:35 +0530

    email verification done

---
+++ djangoproject/authentication/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from .utils import token_generator
+
+# Create your views here.
+class EmailValidationView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'email is invalid'},status = 400)
+        if User.objects.filter(email = email).exists():
+            return JsonResponse({'email_error': 'email already exists'},status = 400)
+        return JsonResponse({'email_valid': True})
+
+
+class UsernameValidationView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'},status = 400)
+        if User.objects.filter(username = username).exists():
+            return JsonResponse({'username_error': 'username already exists'},status = 400)
+        return JsonResponse({'username_valid': True})
+
+class RegistrationView(View):
+    def get(self,request):
+        return render(request,'authentication/register.html')
+    def post(self,request):
+        #GET USER Data
+        # VALIDATE
+        # create a user account
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+        if not User.objects.filter(username = username).exists():
+            if not User.objects.filter(email = email).exists():
+
+                if len(password) < 6:
+                    messages.error(request,'password too short')
+                    return render(request,'authentication/register.html',context)
+            user = User.objects.create_user(username = username, email = email)
+            user.set_password(password)
+            user.is_active = False
+            user.save()
+
+            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+            domain = get_current_site(request).domain
+            #token = token_generator.make_token(user)
+            link = reverse('activate', args = {'uidb64':uidb64,'token': token_generator.make_token(user)})
+            email_subject = "Activate your account"
+
+            activate_url = 'http://'+domain+link
+
+            email_body = "Hi "+user.username + '\n'+" Please use this link to verify your account\n"+ activate_url
+            email = EmailMessage(
+            email_subject,
+            email_body,
+            'noreply@colon.com',
+            [email],
+            )
+            email.send(fail_silently = False)
+            messages.success(request,'Registration Successful')
+            #context = {'uidb64':uidb64,'token': token}
+            return render(request,'authentication/register.html')
+
+        return render(request,'authentication/register.html')
+class VerificationView(View):
+    def get(self,request,uid,token):
+        return redirect('login')
